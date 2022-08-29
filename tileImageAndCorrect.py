7ddd10480f83b94240093f750dcfc7e0b8254457
hossam-zaki <hossam_zaki@brown.edu>
2022-08-29 17:17:55 -0400

    returns bytes for zip file with normalized images

--- tileImageAndCorrect.py
+++ tileImageAndCorrect.py
@@ -3,31 +3,103 @@ Example: Stain normalize with Vahadane algorithm a list of H&E images.
 Paper: "Structure-Preserving Color Normalization and Sparse Stain Separation for Histological Images", Vahadane et al, 2016.
 """
 
+import base64
+from fileinput import filename
+import io
 import os
 import sys
 from glob import glob
+from typing import List
 
+import cv2
 import numpy as np
+from fastapi import FastAPI, File, UploadFile, Response
 from histocartography.preprocessing import VahadaneStainNormalizer
 from histocartography.utils import download_example_data
 from PIL import Image
 from tqdm import tqdm
-from typing import List
-from fastapi import FastAPI, File, UploadFile
+import zipfile
 
 app = FastAPI()
 
 # need to match the first name "files" for each uploaded file
 @app.post("/normalize")
-def normalize_images(files: List[UploadFile] = File(description="Images to process")):
+async def normalize_images(files: List[UploadFile] = File(description="Images to process")):
     """
     Process the images in image path dir. In this dummy example,
     we use the first image as target for estimating normalization
     params.
     """
-    # return {"filename": file.filename}
+    normalize_images = {}
     for file in files:
-        return {"filenames": [file.filename for file in files]}
+        contents = await file.read()
+
+        nparr = np.fromstring(contents, np.uint8)
+
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        img_dimensions = str(img.shape)
+        return_imgs = processImage(img)
+        
+        normalize_images = {file.filename : return_imgs}
+        #return {"normalized" : img[0]}
+        # headers = {'Content-Disposition': 'inline; filename="test.png"'}
+        # return Response(return_imgs[0], headers=headers, media_type='image/png')
+        # return {"files": return_imgs}
+        # return {filename: file.filename}
+
+        # return {"filenames": [file.filename for file in files]}
+    
+    zipFile = zipFiles(normalize_images)
+
+    resp = Response(zipFile.getvalue(), media_type="application/x-zip-compressed", headers={
+        'Content-Disposition': f'attachment;filename=yee.zip'
+    })
+
+    return resp
+
+
+    # headers = {'Content-Disposition': 'inline', 'filename' : "test.png"}
+    # print(return_imgs[1])
+    # return Response(return_imgs[1].tobytes(), headers=headers, media_type='image/png')
+
+def zipFiles(fileDict):
+    zip_filename = "normalized_images.zip"
+
+    s = io.BytesIO()
+
+    zf = zipfile.ZipFile(s, "w")
+
+    for image in fileDict:
+        for count, normalized_image in enumerate(fileDict[image]):
+            zf.writestr(f"{count}_{image}", normalized_image)
+    
+    zf.close()
+
+    return s
+
+def processImage(image):
+
+    normalizer = VahadaneStainNormalizer()
+
+    splitImage = np.array_split(image, 8, axis=1)
+
+    splitImages = []
+
+    for count, tiledImage in enumerate(splitImage):
+        norm_image = normalizer.process(tiledImage)
+
+        # c. save the normalized image
+        # norm_image
+        # norm_image = Image.fromarray(np.uint8(norm_image))
+        # print(norm_image)
+        _, encoded_img = cv2.imencode('.PNG', norm_image)
+
+    # encoded_img = base64.b64encode(encoded_img)
+        splitImages.append(encoded_img)
+    
+    return splitImages
+
     # 1. get image path
     # image_fnames = glob(os.path.join(image_path, '*.png'))
 
